61c79ac4dfee2a8d0e5307196fea7cf622670ec8
hilmi <hilmicihan>
2021-11-29 11:27:22 +0300

    Binary Tree Max PAth Sum

---
+++ cppSolutions/binaryTreeMaxPathSum.cpp
@@ -0,0 +1,57 @@
+/*
+A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
+
+The path sum of a path is the sum of the node's values in the path.
+
+Given the root of a binary tree, return the maximum path sum of any non-empty path.
+link : https://leetcode.com/problems/binary-tree-maximum-path-sum/
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include<bits/stdc++.h>;
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+class Solution
+{
+
+};
+int dfs(TreeNode* root,int& result);
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        int result = root->val; 
+        dfs(root,result);
+        return result;
+        
+    }
+};
+int dfs(TreeNode* root,int& result)
+{
+    if(root==NULL)
+        return 0;
+    // without split
+    int leftMax = dfs(root->left,result);
+    int rightMax = dfs(root->right,result);
+    leftMax  = max(leftMax,0);
+    rightMax = max(rightMax,0);
+    // consider splitting
+    result = max(result,leftMax+root->val+rightMax);
+    return root->val+max(leftMax,rightMax);
+    
+}
