506feb5f482697468ae410332508c330a74eb9b6
hilmi <hilmicihan>
2021-12-07 13:31:27 +0300

    commutable islands

--- cppSolutions/commutableIslands.cpp
+++ cppSolutions/commutableIslands.cpp
@@ -1,10 +1,70 @@
 
-#include<vector>;
+
 using namespace std;
+#include<vector>
 #include<string>
 #include<queue>
 #include<string>
+#include<bits/stdc++.h>
+class Solution {
+int solve(int A, vector<vector<int> > &B);
+};
+// disjoint approach 
+bool compare(vector <int> first, vector <int> second)
+{
+    if (first[2] < second[2])
+        return true;
+    return false;
+}
+
+int find_parent(vector <int> & id,int x)
+{
+    while (id[x] != x)
+    {
+        id[x] = id[id[x]];
+        x = id[x];
+    }
+    return x;
+}
+
+void union1(vector <int> &id, int x, int y)
+{
+    int p = find_parent(id, x);
+    int q = find_parent(id, y);
+    id[p] = id[q];
+}
+
+int Solution::solve(int A, vector<vector<int> > &B) {
+    sort(B.begin(), B.end(), compare);
+    vector <int> id(A + 1);
+    for (int i = 0; i < id.size(); i++)
+        id[i] = i;
+        
+    long long minCost = 0;
+    
+    for (int i = 0; i < B.size(); i++)
+    {
+        vector <int> edge = B[i];
+        int src = edge[0];
+        int dest = edge[1];
+        int weight = edge[2];
+        
+        int par_src = find_parent(id, src);
+        int par_dest = find_parent(id, dest);
+        // cout << "Parent for " << src << " " << par_src << endl;
+        // cout << "Parent for " << dest << " " << par_dest << endl;
+        
+        if (par_src != par_dest)
+        {
+            // cout << "Picked edge from " << src << " " << dest << " " << weight << endl;
+            minCost += weight;
+            union1(id, src, dest);
+        }
+    }
+    return minCost;
+}
 // native approach
+/*
 int minEdge(vector<bool> &visited,vector<vector<int>> &B);
 
 int Solution::solve(int A, vector<vector<int> > &B) {
@@ -46,3 +106,4 @@ int minEdge(vector<bool>& visited,vector<vector<int>> &B)
     return minCost;
 }
 
+*/
