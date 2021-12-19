15935ea23f420e625b1fd9e0aede8274ab484f17
hilmi <hilmicihan>
2021-12-19 12:35:08 +0300

    new solutions array

--- cppSolutions/countingBits.cpp
+++ cppSolutions/countingBits.cpp
@@ -1,6 +1,6 @@
 /*
 Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
-link : https://leetcode.com/problems/counting-bits/
+link : https://leetcode.com/problems/counting-bits/ 
 */
 
 #include<vector>
